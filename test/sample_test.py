4ed2604a8f20ac02a8cb9405165f400f3b7cfd0b
clatran <clatran@ebay.com>
2017-11-15 12:05:46 +1100

    updating with fixtures

--- test/sample_test.py
+++ test/sample_test.py
@@ -7,6 +7,10 @@ from pytest_mock import mocker
 from speedcurve import SpeedCurve
 
 
+@pytest.fixture()
+def url_fixture():
+    return ['http://mocked']
+
 class Source:
     def __init__(self, api_key):
         self.sc = SpeedCurve(api_key=api_key)
@@ -14,9 +18,9 @@ class Source:
 def test_e2e_dummy():
     assert True == True, "This should never happen! OMG! The universe is broken!"
 
-def test_speedcurve(mocker): 
+def test_speedcurve(mocker, url_fixture): 
     source = Source(api_key='abc')
     mocker.patch.object(source.sc, 'sites', autospec=True)
-    source.sc.sites.return_value = ['http://mocked']
-    assert source.sc.sites()[0] == 'http://mocked'
+    source.sc.sites.return_value = url_fixture
+    assert source.sc.sites()[0] == url_fixture[0]
 
